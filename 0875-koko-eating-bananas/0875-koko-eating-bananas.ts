74f2dd5ae929ef7508f57f5c92f7375d1c7afa3f
Karnoark <79748668+karnoark@users.noreply.github.com>
2026-08-29 22:14:23 +0530

    Time: 14 ms (21.88%), Space: 60.7 MB (15.16%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.ts
@@ -0,0 +1,23 @@
+function minEatingSpeed(piles: number[], h: number): number {
+    let total = 0
+    for(let pile of piles){
+        total += pile
+    }
+    function feasible(x: number){
+        let hrs = 0
+        piles.forEach(i => hrs += Math.ceil(i/x))
+        return hrs <= h
+       
+    }
+    let left = 1
+    let right = total
+    while (left < right){
+        let mid = left + Math.floor((right - left)/2)
+        if(feasible(mid)){
+            right = mid
+        }else{
+            left = mid + 1
+        }
+    }
+    return left
+};
